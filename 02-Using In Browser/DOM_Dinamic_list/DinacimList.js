459b90cf5c45f0fea45741652920ae398212dc03
Bogdan <bgroholsky@gmail.com>
2021-02-18 22:06:57 +0200

    Dinamic List add delete insertEnd

--- 02-Using In Browser/DOM_Dinamic_list/DinacimList.js
+++ 02-Using In Browser/DOM_Dinamic_list/DinacimList.js
@@ -1,32 +1,97 @@
+
+
+
+
+
 function CreateNewList(){
 
     let ul = document.createElement('ul');
 
-//ul.addEventListener('click',e => alert(e.target))
+    ul.innerHTML =
+     '<li><span>New list</span>'+
+        '<ul>'+
+            '<li><span>item1</span></li>'+
+            '<li><span>item1</span></li>'+
+        '</ul>'+
+    '</il>';
 
-
-
-
-  ul.innerHTML = '<li>New list'+
-                        '<ul>'+
-                            '<li>item1</li>'+
-                        '</ul>'+
-                 '</il>';
-
+  
     document.body.append(ul);
 
 
 
-    function hide(e){
-        // e.target ссылается на кликнутый <li> элемент
-        // Он отличается от e.currentTarget который будет ссылаться на родительский <ul> в этом контексте
-    //    e.target.style.visibility = 'hidden';
-       alert(e.target.style.innerHTML);
-      }
-      
-      // Назначим обработчик к списку
-      // Он будет вызван когда кликнут на любой <li>
-      ul.addEventListener('click', hide, false);
 
+
+
+
+
+    
+  ul.addEventListener('click',CallOperation);
 
 }
+
+function DefineOperation(){
+    let numberOper = 0;
+    
+    for (const tag of document.getElementsByName('group')) {
+        if(tag.checked) break;               
+            numberOper++;
+    }
+    return numberOper;
+}
+
+const CHANGE_TEXT = 2, DELETE_ELEMENT = 4, ADD_ELEMENT_END = 0;
+
+function CallOperation(event){
+    let numberOperation = DefineOperation();
+
+    switch(numberOperation){
+        case  ADD_ELEMENT_END : 
+       // event.target.parentNode.innerHTML = event.target.parentNode.innerHTML + `<li>${addElemEnd.value}</li>`;
+
+  let lastitem = document.createElement('li');
+    lastitem.innerHTML = `<li>${addElemEnd.value}`;
+        event.target.parentNode.parentNode.append(lastitem);
+                break;
+
+        case  CHANGE_TEXT : '?'
+            event.target.textContent = changeElem.value;
+                break;
+
+        case  DELETE_ELEMENT :           
+        if(event.target.parentNode.parentNode.children.length == 1)
+            event.target.parentNode.parentNode.remove();
+       
+            event.target.parentNode.remove();
+
+            
+                break;
+    }
+
+}
+
+/*
+    let ul = document.createElement('ul');
+let il = document.createElement('il');
+ il.textContent ='fdsfasd';
+ let span = document.createElement('span');
+ span.textContent = 'New list';
+
+
+
+let ul2 = document.createElement('ul');
+let il2 = document.createElement('il');
+il2.textContent ='fdsfasd';
+let span2 = document.createElement('span');
+span2.textContent = 'New item';
+   ul.appendChild(il);  
+    il.appendChild(ul2);
+
+    ul2.appendChild(il2);
+*/
+
+
+  //   ul.addEventListener('click',e =>  e.target.textContent = changeElem.value)
+
+
+  // ul.addEventListener('click', e =>  e.target.parentNode.parentNode.remove());
