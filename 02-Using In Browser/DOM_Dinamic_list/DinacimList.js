935a745343d6d0165dd034fe60feacbd535ea535
Грохольский Богдан <Groh_8npy@od.itstep.local>
2021-02-19 15:50:34 +0200

    insert operation

--- 02-Using In Browser/DOM_Dinamic_list/DinacimList.js
+++ 02-Using In Browser/DOM_Dinamic_list/DinacimList.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 function CreateNewList(){
 
     let ul = document.createElement('ul');
@@ -11,43 +7,58 @@ function CreateNewList(){
      '<li><span>New list</span>'+
         '<ul>'+
             '<li><span>item1</span></li>'+
-            '<li><span>item1</span></li>'+
         '</ul>'+
     '</il>';
 
   
     document.body.append(ul);
-
-
-
-
-
-
-
-
-    
+  
   ul.addEventListener('click',CallOperation);
 
 }
 
 function DefineOperation(){
     let numberOper = 0;
+    let a=0;
     
     for (const tag of document.getElementsByName('group')) {
-        if(tag.checked) break;               
+
+        if(tag.checked) {
+            for (const textFile of document.getElementsByName('textField')) {
+                if(a == numberOper)
+                {
+                    if(textFile.value.length == 0)
+                    {
+                        alert('line is empty');
+                        return -1;
+                    }
+                    else{
+                        return numberOper;
+                    }
+                }
+                a++;
+            }
+            return numberOper;
+        }               
             numberOper++;
     }
+
     return numberOper;
 }
 
-const CHANGE_TEXT = 2, DELETE_ELEMENT = 4, ADD_ELEMENT_END = 0;
+const CHANGE_TEXT = 2, DELETE_ELEMENT = 4, ADD_ELEMENT_END = 0, INSERT = 1, INSER_NEW_LIST = 3;
 
 function CallOperation(event){
     let numberOperation = DefineOperation();
+ 
+
+    
+  if(numberOperation == -1)
+    return;
+    
 
     switch(numberOperation){
         case  ADD_ELEMENT_END : 
-       // event.target.parentNode.innerHTML = event.target.parentNode.innerHTML + `<li>${addElemEnd.value}</li>`;
 
   let lastitem = document.createElement('li');
     lastitem.innerHTML = `<li>${addElemEnd.value}`;
@@ -63,36 +74,39 @@ function CallOperation(event){
         if(event.target.parentNode.parentNode.children.length == 1)
             event.target.parentNode.parentNode.remove();
         else
-            event.target.parentNode.remove();
+            event.target.parentNode.remove();           
+                break;
+
+        case  INSERT : 
+        let il = document.createElement('il');
+        il.innerHTML = `<li><span>${insertText.value}</span></li>`;
+
+            event.target.parentNode.before(il);
+        break;
+
+        case  INSER_NEW_LIST : 
+
+        for (const tag of event.target.parentNode.childNodes) {
+            if(tag.tagName == 'UL'){
 
             
-                break;
+            alert('list have list');
+            return;
+            }
+            
+        }
+       
+
+
+        let newList = event.target.parentNode.innerHTML;
+        newList += '<ul>'+
+        `<li><span>${NewNestedName.value}</span></li>`+
+            '</ul>'+
+        '</il>';
+
+        event.target.parentNode.innerHTML = newList;
+        break;
+                
     }
 
 }
-
-/*
-    let ul = document.createElement('ul');
-let il = document.createElement('il');
- il.textContent ='fdsfasd';
- let span = document.createElement('span');
- span.textContent = 'New list';
-
-
-
-let ul2 = document.createElement('ul');
-let il2 = document.createElement('il');
-il2.textContent ='fdsfasd';
-let span2 = document.createElement('span');
-span2.textContent = 'New item';
-   ul.appendChild(il);  
-    il.appendChild(ul2);
-
-    ul2.appendChild(il2);
-*/
-
-
-  //   ul.addEventListener('click',e =>  e.target.textContent = changeElem.value)
-
-
-  // ul.addEventListener('click', e =>  e.target.parentNode.parentNode.remove());
