929d462516cc331206c117ee7c84e334eee41a93
Грохольский Богдан <Groh_8npy@od.itstep.local>
2021-03-09 15:42:52 +0200

    Forms

--- 05-Forms/01-Forms/JS/Classes.js
+++ 05-Forms/01-Forms/JS/Classes.js
@@ -13,9 +13,8 @@ class Rice{
     get Date(){
         return this.#date;
     }
+    places = new Array(28);
     constructor(direction,date){
-       
-
         this.Direction = direction;
         this.Date = date;
     }
@@ -28,4 +27,32 @@ class PoolRice{
         get ArrRice(){
             return this.#arrRice;
         }
+}
+class Ticket{
+    #date;
+        set Date(value){
+            this.#date = value;
+        }
+        get Date(){
+            return this.#date;
+        }
+    #direction;
+        set Direction(value){
+            this.#direction = value;
+        }
+        get Direction(){
+            return this.#direction;
+        }
+    #place;
+        set Place(value){
+            this.#place = value;
+        }
+        get Place(){
+            return this.#place;
+        }
+    constructor(date,direction,place){
+        this.Date = date;
+        this.Direction = direction;
+        this.Place = place;
+    }
 }
--- 05-Forms/01-Forms/JS/Forms.js
+++ 05-Forms/01-Forms/JS/Forms.js
@@ -1,13 +1,58 @@
 let arrRice = new PoolRice();
-
-
+let arrTickets = new Array();
 function InitPoolRice(){
-    arrRice.ArrRice.push(new Rice('Lviv-Kiev',new Date()));
-    arrRice.ArrRice.push(new Rice('Kiev-Lviv',new Date()));
+    arrRice.ArrRice.push(new Rice('Lviv-Kiev',new Date(2012, 0, 1, 0, 0, 0, 0)));
+    arrRice.ArrRice.push(new Rice('Kiev-Lviv',new Date(2011, 0, 1, 0, 0, 0, 0)));
    
 }
+function CreateTable(){
+    let countPlace;
+    let arrPlace; 
+    for(item = 0; item < arrRice.ArrRice.length; item++){
+        if(SelectDirection.value == arrRice.ArrRice[item].Direction){
+            countPlace = arrRice.ArrRice[item].places;
+            arrPlace = arrRice.ArrRice[item].places;
+        }
+    }
 
 
+    if(document.getElementById('ticketsNum') != undefined){  
+        while (ticketsNum.firstChild) {
+            ticketsNum.removeChild(ticketsNum.firstChild);
+        }
+        mainBlock.removeChild(ticketsNum);
+    }
+
+
+    table = document.createElement('table');
+    table.appendChild(document.createElement('tbody'));
+    table.id = 'ticketsNum';
+
+
+    if(countPlace.length > 2){
+        let Tr = document.createElement('tr');
+        table.firstChild.appendChild(Tr);
+        let Tr2 = document.createElement('tr');
+        table.firstChild.appendChild(Tr2);
+    }
+
+    for(let a = 1; a <= countPlace.length; a++){
+        let Td = document.createElement('td');
+        Td.innerHTML =`<td><input type="checkbox" class="input_data" name="tseremonii">${a}</td>`;
+
+        if(a % 2 == 0){
+            table.firstChild.children[1].appendChild(Td);
+        }
+        else{
+            table.firstChild.children[0].appendChild(Td);
+        }
+        if(arrPlace[a] == 1){
+            Td.firstChild.disabled = true;
+        }
+
+    }
+    totalBlock.before(table);
+}
 function InitLists(){
     InitPoolRice();
     for(let i = 0; i < arrRice.ArrRice.length; i++){ 
@@ -16,10 +61,98 @@ function InitLists(){
         SelectDirection.appendChild(optDir);
     
         let optDate = document.createElement('option');
-        let day = arrRice.ArrRice[i].Date.getDate();
-        let month = arrRice.ArrRice[i].Date.getMonth();
-        let year = arrRice.ArrRice[i].Date.getFullYear();
-        optDate.textContent = (day + '.' + month +'.' + year);
+        optDate.textContent = TakeDateInt(arrRice.ArrRice[i].Date);
         SelectDate.appendChild(optDate);
     }
+
+    document.getElementById('SelectDirection').onchange = ChangeSelectDate;
+//    ChangeSelectDate();
+    totalPrice = totalPriceVal;
 }
+
+let totalPriceVal = 40;
+function ChangeSelectDate(){
+
+    while (SelectDate.firstChild) {
+        SelectDate.removeChild(SelectDate.firstChild);
+      }
+
+    for(item of arrRice.ArrRice){
+        if(item.Direction == SelectDirection.value){
+            let Option = document.createElement('option');
+            Option.label = TakeDateInt(item.Date);
+            SelectDate.appendChild(Option);
+        }
+    }
+    totalPrice.textContent = totalPriceVal;
+CreateTable();
+RefreshTickets();
+
+}
+function TakeDateInt(date){
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    return (day + '.' + month +'.' + year);
+}
+function BuyBook(){
+
+    let numberPlace;
+    let countChoicePlace = 0;
+    let choicePlace;
+
+    for(item of ticketsNum.firstChild.children){
+        for(itemTr of item.children){
+            if(itemTr.firstChild.checked && itemTr.firstChild.disabled == false){
+                countChoicePlace++;
+                if(countChoicePlace >= 2){
+                    alert('Choice only one tickets');
+                    choicePlace.disabled = false;
+                    return;
+                }
+                choicePlace = itemTr.firstChild;
+                itemTr.firstChild.disabled = true;
+                numberPlace = itemTr.textContent;         
+                totalPrice.textContent += totalPriceVal;               
+            }
+        }
+    }
+    for(item = 0; item < arrRice.ArrRice.length; item++){
+        if(SelectDirection.value == arrRice.ArrRice[item].Direction){
+           arrRice.ArrRice[item].places[numberPlace] = 1;
+        }
+    }
+    let newTickets = new Ticket(SelectDate.children[0].label,SelectDirection.value,numberPlace);
+    arrTickets.push(newTickets);
+    AddTickets(arrTickets[arrTickets.length - 1]);
+}
+
+function RefreshTickets(){
+
+    while (myTicketsTable.children[0].children.length != 1) {
+        myTicketsTable.children[0].removeChild(myTicketsTable.children[0].lastChild);
+      }
+
+    for(tickets of arrTickets){
+        if(tickets.Direction == SelectDirection.value){
+            AddTickets(tickets);
+        }
+    }
+}
+function AddTickets(tickets){
+    let Tr = document.createElement('tr');
+    let TdDirection = document.createElement('td');
+    TdDirection.textContent = tickets.Direction;
+    Tr.append(TdDirection);
+
+    let TdDate = document.createElement('td');
+    TdDate.textContent = tickets.Date;
+    Tr.append(TdDate);
+
+    let TdPlace = document.createElement('td');
+    TdPlace.textContent = tickets.Place;
+    Tr.append(TdPlace);
+
+    myTicketsTable.lastChild.appendChild(Tr);
+}
+
