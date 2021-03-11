91fd60c4bba1ae8a89389943c8cae942f08d57c9
Грохольский Богдан <Groh_8npy@od.itstep.local>
2021-03-11 12:01:59 +0200

    Form2 tast 1 did

--- 04-BOM/Gallery/Gallery.js
+++ 04-BOM/Gallery/Gallery.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function ShowInfoPicture(picture){
   let path = picture.currentSrc;
   let startNameIndex = path.lastIndexOf('/') + 1;
@@ -7,18 +6,7 @@ function ShowInfoPicture(picture){
   let message = `Name: ${name} Width: ${picture.clientWidth}\nHeight: ${picture.clientHeight}`;
   document.title = message;
 }
-function OpenPicture(){
-=======
-function ShowInfoPicture(){
-  let widthPicture = event.target.clientWidth;
-  let heighyPicture = event.target.clientHeight;
-  let namePicture = event.target.currentSrc.slice(event.target.currentSrc.lastIndexOf('/') + 1,event.target.currentSrc.lastIndexOf('.'));
-  let messageLine = `Name:${namePicture} Width: ${widthPicture}\nHeight: ${heighyPicture}`;
-  document.title = messageLine;
-}
-
 function OpenPicture(){ 
->>>>>>> cf71cdc6b7e81e008687ad3627c69ebc5376c62b
   let currSrcPicture = event.target.currentSrc;
   let path = currSrcPicture.replace('/SMALL','');
   window.open('Picture.html?' + path, 'example', "width=300,height=300");
--- 05-Forms/02-Forms/Froms.js
+++ 05-Forms/02-Forms/Froms.js
@@ -1,7 +1,7 @@
-function Reset(container){
+function ResetForm(container){
     for(item of container.children){
         if(item.nodeName == 'DIV'){
-            Reset(item);
+            ResetForm(item);
         }
         if(item.nodeName == 'INPUT'){
             if(item.type == 'text'){
@@ -16,21 +16,115 @@ function Reset(container){
         }
     }
 }
+let gFullTxt = true;
+let gChoiceRadio = false;
+let gCheckBox = 0;
+let gSelected = false;
+function ResetCheckFilds(){
+    gChoiceRadio = false;
+    gFullTxt = true;
+    gCheckBox = 0;
+    gSelected = false;
+}
+function sendForm(container){
+    CheckForm(container);
+    let messageError = CreateMessageError();
+    if(messageError.length != 0){
+        alert(messageError);
+    }
+    else{
+        registrBlock.hidden = true;
+        tableData.hidden = false;
+        FillTable();
+    }  
+    ResetCheckFilds();
+}
+let inputDataUser = new String();
 function CheckForm(container){
     for(item of container.children){
         if(item.nodeName == 'DIV'){
             CheckForm(item);
+            continue;
         }
         if(item.nodeName == 'INPUT'){
             if(item.type == 'text'){
                 if(item.value.length == 0){
-                    item.style.backgroundColor = 'red';
+                    gFullTxt = false
+                    break;
+                }
+                else{
+                    if(item.id != 'password' && item.id != 'confirmPasswd')
+                        inputDataUser += item.value + '/';
                 }              
             }
-            else{
-                item.style.backgroundColor = 'red';
+            else if(item.type == 'password'){
+                if(item.value.length == 0){
+                    gFullTxt = false
+                    break;
+                }
+                else{
+                    if(item.id != 'password' && item.id != 'confirmPasswd')
+                        inputDataUser += item.value + '/';
+                }              
             }
-            
+            else if(item.type == 'radio'){
+               if(item.checked){
+                 gChoiceRadio = true;
+                 let sex;
+                    if(item.nextSibling.data[0] == 'M'){
+                        inputDataUser += 'Man';
+                    }
+                    else{
+                        inputDataUser += 'Women';
+                    }
+                 inputDataUser += '/';               
+                }                                
+            }
+            else if(item.type == 'checkbox'){
+                if(item.checked){
+                 gCheckBox++;
+                 if(gCheckBox == 1){
+                    inputDataUser += item.nextSibling.textContent + '/';
+                 }
+                }                                    
+             }            
         }
+        if(item.nodeName == 'SELECT'){
+            if(item.selectedIndex != 0){
+                gSelected = true;
+                inputDataUser += item.value + '/';
+            }
+        }
+    }
+}
+const MIN_LENGTH_PASSWD = 3,MAX_LENGTH_PASSWD = 10;
+function CreateMessageError(){
+    let messageError = new String();
+    if(!gFullTxt){
+        messageError += 'Not all fields is full\n';
+    }
+    if(!gChoiceRadio){
+        messageError += 'Choice radio\n';
+    }   
+    if(gCheckBox > 1 || gCheckBox == 0){
+        messageError += 'Incorrect count choice checkBox\n';
+    }
+    if(!gSelected){
+        messageError += 'Position is not choice';
+    }
+    if(password.value.length < MIN_LENGTH_PASSWD || password.value.length > MAX_LENGTH_PASSWD){
+        messageError += '\nIncorrect length password';
+    }
+    if(password.value != confirmPasswd.value){
+        messageError += '\nConfirm password unlike';
+    }
+    return messageError;
+}
+function FillTable(){
+
+    for(tr of tableData.firstElementChild.children){
+        let cutPartStr = inputDataUser.substring(0,inputDataUser.indexOf('/'));
+        tr.children[1].textContent = cutPartStr;
+        inputDataUser = inputDataUser.replace(cutPartStr + '/','');
     }
 }
