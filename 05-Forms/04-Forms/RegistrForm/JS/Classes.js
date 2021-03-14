28f531e52bfa49d2953c0eecdd092c7ac688c232
Bogdan <bgroholsky@gmail.com>
2021-03-14 19:59:34 +0200

    form 4

--- 05-Forms/04-Forms/RegistrForm/JS/Classes.js
+++ 05-Forms/04-Forms/RegistrForm/JS/Classes.js
@@ -1,27 +1,66 @@
 class CheckForm{
-    #patrnEmail = new RegExp();
+    #patrnEmail = new RegExp();  
         set #PatrEmail(value){
-        this.#patrnEmail = value;   
+            this.#patrnEmail = value;   
         }
         get PatrEmail(){
             return this.#patrnEmail;
         }
+    #patrnIncleInt = new RegExp();
+        set #PatrnIncleInt(value){
+            this.#patrnIncleInt = value;   
+        }
+        get PatrnIncleInt(){
+            return this.#patrnIncleInt;
+        }
     constructor(){
-        //@+[a-z]+\.[a-z]
-        this.#PatrEmail = /[a-z]{3}/i;
+        this.#PatrEmail = /[a-z_.-\^\d]{3}@+[a-z_.-\^\d]+\.[a-z_.-\^\d]/i;
+        this.#PatrnIncleInt = /[\d]/i;
+        this.#PatrnPasswd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{3,}$/;
+        this.#SMinLengthPsswd = 6;
     }
-    CheckEmail(data){
-       let a = this.PatrEmail.test(data);
-       alert(a);
+    CheckEmail(data){  
         if(typeof data != 'string'){
             throw 'Incorect Email';
         }
         else if(!data.length){
-            throw 'Email cann`t be empty';
+            return 'Email can`t be empty';
         }
-       if(this.PatrEmail.test(data)){
-         //  throw 'Incorrect email';
-       }
-      
+        else if(this.PatrnIncleInt.test(data)){
+            return 'Email can`t includes numbers';
+        }
+        else if(!this.PatrEmail.test(data)){
+            return 'Incorrect form email';
+        }   
+        return ''; 
+    }
+    #patrnPasswd = new RegExp();
+        set #PatrnPasswd(value){
+            this.#patrnPasswd = value;   
+        }
+        get PatrnPasswd(){
+            return this.#patrnPasswd;
+        }
+    #sMinLengthPsswd;
+        set #SMinLengthPsswd(value){
+            this.#sMinLengthPsswd = value;   
+        }
+        get SMinLengthPsswd(){
+            return this.#sMinLengthPsswd;
+        }
+    CheckPsswd(data){  
+        if(data.length < this.SMinLengthPsswd){
+            return 'Password must be more 6 symbol';
+        }
+        if(!this.PatrnPasswd.test(data)){
+            return 'Incorrect password';
+        }
+        return '';
+    }
+    CheckConfirmPsswd(psswd,confPsswd){
+        if(psswd != confPsswd){
+            return 'password must mutch';
+        }
+        return '';
     }
 }
--- 05-Forms/04-Forms/RegistrForm/JS/RegistrForm.js
+++ 05-Forms/04-Forms/RegistrForm/JS/RegistrForm.js
@@ -1,11 +1,55 @@
 let gCheckObj = new CheckForm();
-
+const cSeparete = ';';
 function CheckInputDate(){
    try{
-      gCheckObj.CheckEmail(email.value);
+      errEmail.textContent = gCheckObj.CheckEmail(email.value);
+      errPassword.textContent = gCheckObj.CheckPsswd(password.value);
+      errConfirmPsswd.textContent = gCheckObj.CheckConfirmPsswd(password.value,confirmPsswd.value);
    }
-   catch(message){
-      alert(message);
+   catch(err){
+      alert(err);
    }
+  SaveUserData();
    return false;
 }
+function SaveUserData(){
+   for(errLabel of document.getElementsByClassName('errLabel')){
+      if(errLabel.textContent){
+         return false;
+      }
+   }
+   for(inptElem of document.getElementsByTagName('input')){
+      document.cookie = `${inptElem.id}=${inptElem.value}${cSepareta}`;
+   }
+}
+function LoadCookie(){
+   let DataArr = document.cookie.split(cSeparete);
+   let indexCook = 0;
+   for(inptElem of document.getElementsByTagName('input')){
+      if(inptElem.type == 'text'){
+         inptElem.value = DataArr[indexCook].substring(DataArr[indexCook].indexOf('=') + 1);
+      }
+      else if(inptElem.type == 'password'){
+         inptElem.textContent = DataArr[indexCook];
+      }
+      indexCook++;
+   }
+   registBlock.hidden = true;
+   CloseRegWin();
+  // document.location.href = '/UserInfo.html';
+}
+function deleteCookie(name) {
+   var cookies = document.cookie.split(";");
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i];
+		var eqPos = cookie.indexOf("=");
+		var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+		document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+	}
+ }
+ function CloseRegWin(){
+   registForm.hidden = true;
+   registBlock.style.width = 0;
+   registBlock.style.padding = 0;
+ }
