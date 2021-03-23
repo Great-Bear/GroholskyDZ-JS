85fc8404c489194b1dca7dbed262f2646c9fd76d
Bogdan <bgroholsky@gmail.com>
2021-03-23 12:51:54 +0200

    jQuery

--- JS/09-JQuery/Task Two/Task.js
+++ JS/09-JQuery/Task Two/Task.js
@@ -1,47 +1,82 @@
 let posY;
-let timer;
 let change = false;
-let gevent;
+let changeLeft = false;
+let gCorrectLeft = {
+    valueTop:0,
+    valueBottom:0
+}
+let gCorrectRight = {
+    valueTop:0,
+    valueBottom:0
+}
 $(document).ready(function() {
     $('#controlPoint').mousedown(function(event){
         posY = event.pageY;    
-        change = !change;  
-        gevent = event;
-        str = elemid.clientHeight
-        str2 = elemid2.clientHeight
+        changeLeft = true;  
+        gCorrectLeft.valueTop = elemid.clientHeight
+        gCorrectLeft.valueBottom = elemid2.clientHeight
     })    
-    $(document.body).mouseup(function(event){
-        //change = !change
+    $('#leftPart').mouseup(function(event){
+        changeLeft = false
     })  
-    $(document.body).mousemove(function(event){
-        if(change)
-        ChangePos(event);
+    $('#rightPart').mouseover(function(event){
+        changeLeft = false
     })  
-    $('#controlPoint').mousemove(function(event){
-       // if(change)
-       // ChangePos(event);
-      })      
-});
-let str = 0;
-let str2 = 0 ;
-let step = 0.01;
-let maxHeid= 300;
-function ChangePos(event){
-   if(posY < event.pageY){
-       if(elemid2.clientHeight <= 5 ){
-           return;
-       }
-    str += event.pageY - posY;
-    str2 -= event.pageY - posY;
-    elemid.style.height = str + 'px';
-    elemid2.style.height = str2 + 'px';
-   }
-   else{  
-    str -= posY - event.pageY ;
-    elemid.style.height = str + 'px';
-    str2 += posY - event.pageY ;
-    elemid2.style.height = str2 + 'px';
-   }
-   posY = event.pageY;
+    $('#leftPart').mousemove(function(event){
+        if(changeLeft)
+        ChangeSizeBlocks(event,gCorrectLeft,elemid.id,elemid2.id);
+    })   
 
+    $('#controlPointRight').mousedown(function(event){
+        posY = event.pageY;    
+        change = true;  
+        gCorrectRight.valueTop = elemRUp.clientHeight;
+        gCorrectRight.valueBottom = elemRDo.clientHeight;
+    ;})    
+    $('#rightPart').mouseup(function(event){
+        change = false
+    })  
+    $('#leftPart').mouseover(function(event){
+        change = false
+    })  
+    $('#rightPart').mousemove(function(event){
+        if(change)
+        ChangeSizeBlocks(event,gCorrectRight,elemRUp.id,elemRDo.id);
+    })   
+    
+      $('#triangleLeft').click(function() {
+        $( "#leftPart" ).animate({width: 'toggle'});
+        $( "#hiddenSlider" ).animate({left: '0%'});
+        $( "#rightPart" ).animate({width: '100%'});
+        $('#triangleLeft').hide();
+        $('#triangleRight').show('false');
+      });
+      $('#triangleRight').click(function() {
+        $( "#leftPart" ).animate({width: 'toggle'});
+        $( "#hiddenSlider" ).animate({left: '25%'});
+        $('#triangleLeft').show();
+        $('#triangleRight').hide('false');
+        $( "#rightPart" ).animate({width: '75%'});
+      });
+       
+});
+const cMinHeight = 100;
+function ChangeSizeBlocks(event,gCorrectLeft,topBlockId,bottomnBlockId){
+   if(posY < event.pageY){
+    if(gCorrectLeft.valueBottom < cMinHeight){ 
+        return
+    }
+    gCorrectLeft.valueTop += event.pageY - posY;
+    gCorrectLeft.valueBottom -= event.pageY - posY ;
+   }
+   else{   
+    if(gCorrectLeft.valueTop < cMinHeight){ 
+        return
+    }
+    gCorrectLeft.valueTop -= posY - event.pageY ;
+    gCorrectLeft.valueBottom += posY - event.pageY ;
+   }
+   $(`#${topBlockId}`).css('height',gCorrectLeft.valueTop + 'px');
+   $(`#${bottomnBlockId}`).css('height',gCorrectLeft.valueBottom + 'px');
+   posY = event.pageY;
 }
